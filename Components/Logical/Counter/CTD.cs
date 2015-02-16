f5564dd21a556bca6cf966a941857bbd8c803352
Allan Leon Alencar Leitão <alfaallan@gmail.com>
2015-02-16 14:45:05 -0300

    Implemented CTD and CTU

---
+++ Components/Logical/Counter/CTD.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Components.Logical
+{
+    /// <summary>
+    /// Component: Count Donw
+    /// Description: Raising edge counter
+    /// Function: Counts minus one for each raising edge detected, while counter is bigger or equals its limit, it will give high output
+    /// </summary>
+    class CTD : NameableComponentBase
+    {
+        #region Properties
+        /// <summary>
+        /// Counter limit value
+        /// </summary>
+        public int Limit
+        {
+            get { return _Limit; }
+            set
+            {
+                _Limit = value;
+                RaisePropertyChanged("Limit");
+            }
+        }
+
+        /// <summary>
+        /// Counter current value
+        /// </summary>
+        public int CurrentValue
+        {
+            get { return _CurrentValue; }
+            set
+            {
+                _CurrentValue = value;
+                RaisePropertyChanged("CurrentValue");
+            }
+        }
+        #endregion Properties
+
+        #region Functions
+        protected override void RunLogicalTest()
+        {
+            if (!LastInput && LeftLide.LogicLevel) CurrentValue--;
+            LastInput = LeftLide.LogicLevel;
+            InternalState = (CurrentValue >= Limit);
+        }
+        #endregion Functions
+
+        #region Constructors
+        public CTD()
+        {
+            
+        }
+
+        public CTD(Node Left, Node Right)
+            : base(Left, Right)
+        {
+
+        }
+
+        public CTD(int startValue, Node Left, Node Right)
+            : this(Left, Right)
+        {
+            CurrentValue = startValue;
+        }
+
+        public CTD(string name, Node Left, Node Right)
+            : base(name, Left, Right)
+        {
+
+        }
+
+        public CTD(string name, int startValue, Node Left, Node Right)
+            : base(name, Left, Right)
+        {
+            CurrentValue = startValue;
+        }
+        #endregion Constructors
+
+        #region Internal Data
+        bool LastInput;
+        int _Limit;
+        int _CurrentValue;
+        #endregion Internal Data
+    }
+}
---
+++ Components/Logical/Counter/CTU.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Components.Logical
+{
+    /// <summary>
+    /// Component: Count Up
+    /// Description: Raising edge counter
+    /// Function: Counts plus one for each raising edge detected, once counter is bigger or equals its limit, it will give high output
+    /// </summary>
+    class CTU : NameableComponentBase
+    {
+        #region Properties
+        /// <summary>
+        /// Counter limit value
+        /// </summary>
+        public int Limit
+        {
+            get { return _Limit; }
+            set
+            {
+                _Limit = value;
+                RaisePropertyChanged("Limit");
+            }
+        }
+
+        /// <summary>
+        /// Counter current value
+        /// </summary>
+        public int CurrentValue
+        {
+            get { return _CurrentValue; }
+            set
+            {
+                _CurrentValue = value;
+                RaisePropertyChanged("CurrentValue");
+            }
+        }
+        #endregion Properties
+
+        #region Functions
+        protected override void RunLogicalTest()
+        {
+            if (!LastInput && LeftLide.LogicLevel) CurrentValue++;
+            LastInput = LeftLide.LogicLevel;
+            InternalState = (CurrentValue >= Limit);
+        }
+        #endregion Functions
+
+        #region Constructors
+        public CTU()
+        {
+            
+        }
+
+        public CTU(Node Left, Node Right)
+            : base(Left, Right)
+        {
+
+        }
+
+        public CTU(int startValue, Node Left, Node Right)
+            : this(Left, Right)
+        {
+            CurrentValue = startValue;
+        }
+
+        public CTU(string name, Node Left, Node Right)
+            : base(name, Left, Right)
+        {
+
+        }
+
+        public CTU(string name, int startValue, Node Left, Node Right)
+            : base(name, Left, Right)
+        {
+            CurrentValue = startValue;
+        }
+        #endregion Constructors
+
+        #region Internal Data
+        bool LastInput;
+        int _Limit;
+        int _CurrentValue;
+        #endregion Internal Data
+    }
+}
