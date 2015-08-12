85bb404f99442f9602449fcb2547952e7eb80591
Allan Leon A. Leitão <alfaallan@gmail.com>
2015-08-12 09:57:31 -0300

    Removed unsed usings

--- Core/Structure/Diagram.cs
+++ Core/Structure/Diagram.cs
@@ -1,11 +1,7 @@
-﻿using Core.Components;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
 
 namespace Core.Components
 {
